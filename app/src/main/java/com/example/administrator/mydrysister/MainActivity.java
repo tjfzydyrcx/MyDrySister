02453b5e9bb26669c12cdeda11ce64c1e6fe4ef3
不将就 <3083147182@qq.com>
2018-01-31 10:46:56 +0800

    第二次提交文件

--- app/src/main/java/com/example/administrator/mydrysister/MainActivity.java
+++ app/src/main/java/com/example/administrator/mydrysister/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * 我是来上传文件的
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
